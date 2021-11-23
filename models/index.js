c1fe31ec56622d818c92035a994a7fc51105ca38
Phon Phongsavath <pphongsavath03@gmail.com>
2021-11-23 01:21:02 -0500

    set up models

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Post = require('./Post');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
+module.exports = { User, Post, Comment };
